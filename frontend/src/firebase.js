65741ec6699437285ff9248058974091b9df174b
hrajput0322 <hrajput0322@gmail.com>
2024-07-03 23:04:45 +0530

    first commit

---
+++ frontend/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2kwo5LnJ9y_lfGyz_qSockmDPk2qiydM",
+  authDomain: "e-learning-61728.firebaseapp.com",
+  projectId: "e-learning-61728",
+  storageBucket: "e-learning-61728.appspot.com",
+  messagingSenderId: "837706312318",
+  appId: "1:837706312318:web:fe818433d5951f96f1462f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
